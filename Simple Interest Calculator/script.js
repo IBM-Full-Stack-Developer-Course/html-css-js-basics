94a1f3f663e094a767985bfc47ea8d13015276bf
Attila Orosz <attila.orosz.dev@gmail.com>
2025-07-29 12:52:41 +0400

    Small improvements to the exercise "Simple Interest Calcualtor"

--- Simple Interest Calculator/script.js
+++ Simple Interest Calculator/script.js
@@ -1,30 +1,39 @@
 function compute() {
-    let p = document.getElementById("principal").value;
+    // let p = document.getElementById("principal").value; // This came with the starter code, no point keeping it
+
+    // This was part of the exercise, so keeping it
     let result = document.getElementById("result")
 
-    let principal = document.getElementById("principal").value
-    let rate = document.getElementById("rate").value
-    let years = document.getElementById("years").value
+    // Values read from various elements, parsed to ensure type safety
+    var principal = parseInt(document.getElementById("principal").value);
+    var rate = parseFloat(document.getElementById("rate").value);
+    var years = parseInt(document.getElementById("years").value);
 
-    if (principal <= 0 ) {
-
-        alert("Enter a positive number!")
+    // Validate principal, it shyoudl be a number above zero
+    if (isNaN(principal) || principal <= 0) {
+        alert("Enter a positive number for Principal");
         document.getElementById("principal").focus();
-
-    } else{
-        /**
-         * Even though the lab said only do the display bit here, I suppose there is no point of completing the
-         * calculations either, if te principal is not valid.
-         */
-        let interest = principal * years * rate / 100
-        let amount = parseInt(principal) + parseFloat(interest)
-        let year = new Date().getFullYear() + parseInt(years);
-
-        result.innerHTML=`If you deposit $<mark>${principal}</mark>,<br>` +
-                         `at an interest rate of <mark>${rate}</mark>%.<br>` +
-                         `You will receive an amount of $<mark>${amount}</mark>,<br>` +
-                         `in the year <mark>${year}</mark><br>`
+        return;
     }
+
+    // Validate years to be a number
+    if (isNaN(years) || years <= 0) {
+        alert("Enter a positive number for Years");
+        document.getElementById("years").focus();
+        return;
+    }
+
+    // Calculate everything
+    let interest = principal * years * rate / 100
+    let amount = principal + interest
+    let year = new Date().getFullYear() + years;
+
+    // Display the results
+    result.innerHTML=`If you deposit $<mark>${principal}</mark>,<br>` +
+                     `at an interest rate of <mark>${rate}</mark>%.<br>` +
+                     `You will receive an amount of $<mark>${amount}</mark>,<br>` +
+                     `in the year <mark>${year}</mark><br>`
+
 
 
 
